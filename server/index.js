144b37a832d1155d4d56a9ae8515840e7f82eb1c
Smith <smith.dbritto8@gmail.com>
2022-11-17 11:19:50 +0530

    Removed mongodb commented credentials

--- server/index.js
+++ server/index.js
@@ -19,12 +19,10 @@ app.use("/posts",postRoutes);
 app.get('/',(req,res)=>{
     res.send("Hello!");
 });
-//Percentage encoding is used in password as @ or # can't be used in password section in link
-//const CONNECTION_URL='mongodb+srv://SmithD:Smith%40555@mern-memories.2hrfwsl.mongodb.net/?retryWrites=true&w=majority';
+
 const PORT=process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=>{app.listen(PORT,()=>{console.log(`Server running on Port ${PORT}`)})})
     .catch((error)=>{console.log(error.message)})
 
-// mongoose.set('useFindAndModify',false);
